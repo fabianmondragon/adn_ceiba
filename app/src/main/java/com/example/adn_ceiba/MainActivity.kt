3c73b4369a9ffdf9fef5a6342ee2bc4b28c2809c
Fabián Alejandro Mondragón Erazo - Ceiba Software <fabian.mondragon@ceiba.com.co>
2020-01-29 22:04:40 -0500

    add: initial project

---
+++ app/src/main/java/com/example/adn_ceiba/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.adn_ceiba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
